5c9c44ed0bfd1a1a3e7efdfde3e0688638c7fe61
Eze David <166126231+7thKingDavid@users.noreply.github.com>
2024-08-15 01:37:05 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const elements = document.querySelectorAll('.animated');
+    
+    function isInViewport(element) {
+      const rect = element.getBoundingClientRect();
+      return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+      );
+    }
+  
+    function handleScroll() {
+      elements.forEach(element => {
+        if (isInViewport(element)) {
+          element.classList.add('visible');
+        }
+      });
+    }
+  
+    window.addEventListener('scroll', handleScroll);
+    handleScroll(); // Initial check
+  });
+  
